f4cffe28e30deeb44beecabf29a8acbd79aba752
Darren Ford <dford_au-reg@yahoo.com>
2020-11-30 07:16:22 +1100

    Further work on xcode-demo

--- Demo/xcode-pane-example/panes/FramePropertyPaneViewController.swift
+++ Demo/xcode-pane-example/panes/FramePropertyPaneViewController.swift
@@ -8,11 +8,23 @@
 
 import Cocoa
 
+import DSFStepperView
+import DSFToggleButton
+
 class FramePropertyPaneViewController: NSViewController {
+	@IBOutlet weak var width: DSFStepperView!
+	@IBOutlet weak var height: DSFStepperView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
-    
+
+	@IBAction func toggled(_ sender: DSFToggleButton) {
+		let enabled = (sender.state == .on)
+
+		self.width.isEnabled = enabled
+		self.height.isEnabled = enabled
+	}
+
 }
